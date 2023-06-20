579f7d94b961859f10a46b06fad32535fc023b10
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2023-06-20 10:55:57 +0700

    Create BeratBadanByPanjangBadanTinggiBadan.php

---
+++ app/Models/BeratBadanByPanjangBadanTinggiBadan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BeratBadanByPanjangBadanTinggiBadan extends Model
+{
+    use HasFactory;
+    protected $table = 'standard_antropometri_pb_by_umur';
+    protected $fillable = [];
+}
