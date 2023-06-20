c4f10c30fc3957aa7faaf5adbbe06d74ec5939ca
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2023-06-20 17:04:12 +0700

    Update StatistikController.php

--- app/Http/Controllers/Global/StatistikController.php
+++ app/Http/Controllers/Global/StatistikController.php
@@ -11,42 +11,44 @@ class StatistikController extends Controller
 {
     public function __invoke()
     {
-       return view('guest.statistik.grafik',[
-        'kab_kota' => KabupatenKota::all(),
-       ]);
+        return view('guest.statistik.grafik', [
+            'kab_kota' => KabupatenKota::all(),
+        ]);
     }
-    public function getByKabKota(){
+    public function getByKabKota()
+    {
         $kabKota = \request()->kab_kota_id ?? null;
         $pengukuran = Pengukuran::with('anak.orangTua.kelurahanDesa.kecamatan.kabupatenKota')
-        ->whereHas('anak.orangTua.kelurahanDesa.kecamatan.kabupatenKota',function($query) use($kabKota){
-            return $query->where('id',$kabKota);
-        })->where('bb_zscore','<=','-3');
+            ->whereHas('anak.orangTua.kelurahanDesa.kecamatan.kabupatenKota', function ($query) use ($kabKota) {
+                return $query->where('id', $kabKota);
+            });
         $total_kasus = 0;
         $jk_l = 0;
         $jk_p = 0;
-        foreach($pengukuran->get() as $data) {
-            $total_kasus++;
-            if($data->anak->jenis_kelamin === 'L') {
-                $jk_l++;
-            } else if($data->anak->jenis_kelamin === 'P') {
-                $jk_p++;
+        foreach ($pengukuran->get() as $data) {
+            if ($data->z_score <= -3) {
+                $total_kasus++;
+                if ($data->anak->jenis_kelamin === 'L') {
+                    $jk_l++;
+                } else if ($data->anak->jenis_kelamin === 'P') {
+                    $jk_p++;
+                }
             }
-
-        } 
+        }
         return response()->json([
             'data' => [
                 [
                     'x' => 'TOTAL KASUS',
                     'y' => $total_kasus,
                 ],
-              [
-                  'x' => 'LAKI LAKI',
-                  'y' => $jk_p,
-              ],[
-                  'x' => 'PEREMPUAN',
-                  'y' => $jk_l,
-              ],
-              
+                [
+                    'x' => 'LAKI LAKI',
+                    'y' => $jk_p,
+                ], [
+                    'x' => 'PEREMPUAN',
+                    'y' => $jk_l,
+                ],
+
             ]
         ]);
     }
