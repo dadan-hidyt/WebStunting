c8d519d759ce32b60902a034ab4e8e58b5e408c9
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2023-06-20 10:56:02 +0700

    Create 2023_06_20_105514_create_berat_badan_by_panjang_badan_tinggi_badans_table.php

---
+++ database/migrations/2023_06_20_105514_create_berat_badan_by_panjang_badan_tinggi_badans_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berat_badan_by_panjang_badan_tinggi_badans', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('berat_badan_by_panjang_badan_tinggi_badans');
+    }
+};
