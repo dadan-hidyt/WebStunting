929caf14e20f677b9cfc7588e53ae16c9bd2729e
Dadan Hidayat <80499441+dadan-hidyt@users.noreply.github.com>
2023-06-21 12:33:45 +0700

    Update grafik.blade.php

--- resources/views/guest/statistik/grafik.blade.php
+++ resources/views/guest/statistik/grafik.blade.php
@@ -7,11 +7,15 @@
     <title>Info Grafik</title>
     <style>
         .card,.card-header,.card-body {
-            border-radius: 5px!important;
-            border: none;
+            border-radius: 10px!important;
+            border: none!important;
+        }
+        .card-header{
+            box-shadow: 0px 0px 2px #a1a1a1 !important;
         }
         .card{
             box-shadow: 0px 0px 8px #dedede!important;
+
         }
         </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
