0c7ce7c97cf7bd1d439aaa31eb4299bda90a821e
Dadan Hidayat <dadanhidyt@gmail.com>
2023-06-20 21:07:25 -0700

    app

--- resources/views/guest/statistik/grafik.blade.php
+++ resources/views/guest/statistik/grafik.blade.php
@@ -5,6 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Info Grafik</title>
+    <style>
+        .card,.card-header,.card-body {
+            border-radius: 5px!important;
+            border: none;
+        }
+        .card{
+            box-shadow: 0px 0px 8px #dedede!important;
+        }
+        </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
